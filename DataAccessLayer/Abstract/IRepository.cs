7d2619e2a584598f21928b98f34bfadb0bdbb5fe
melike yıldırım <melikeyildirim718@gmail.com>
2021-05-15 20:03:16 +0300

    Kategori Silme ve Güncelleme

--- DataAccessLayer/Abstract/IRepository.cs
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -14,6 +14,8 @@ namespace DataAccessLayer.Abstract
         void Insert(T p);
 
         void Update(T p);
+
+        T Get(Expression<Func<T,bool>> filter);
         void Delete(T p);
 
         List<T> List(Expression<Func<T, bool>> filter);
